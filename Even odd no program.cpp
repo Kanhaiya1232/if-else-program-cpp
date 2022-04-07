8002175394d291f67ebff05f59ec239e42e0799e
Kanhaiya1232 <101667615+Kanhaiya1232@users.noreply.github.com>
2022-04-07 19:08:16 +0530

    Add files via upload

---
+++ Even odd no program.cpp
@@ -0,0 +1,16 @@
+//even/odd no program
+#include<iostream>
+using namespace std;
+int main()
+{
+int num=13;
+if(num %2==0)
+{
+    cout<<"Even num";
+}
+    else
+    {
+        cout<<"Odd no";
+    }
+    return 0;
+}
